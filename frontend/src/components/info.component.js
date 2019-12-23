a3203cc7317ce7a834f426ba6828b9fb871cc1d9
baileyg2016 <bspell20@vt.edu>
2019-12-23 10:14:55 -0500

    getting the account balance but the grid is not working well

--- frontend/src/components/info.component.js
+++ frontend/src/components/info.component.js
@@ -5,21 +5,33 @@ export default class Info extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          html: ''
+          html: '',
+          balance: 0
         };
     }
 
     componentDidMount() {
         var content = '<tr><td><strong>Name</strong></td><td><strong>Amount</strong></td><td><strong>Date</strong></td></tr>';
-        axios.get("http://localhost:5000/transactions").then(res => {
-            res.data.transactions.transactions.forEach(function(txn, idx) {
+        axios.get("http://localhost:5000/balance").then(res => {
+            res.data.balance.accounts.forEach((account, idx) => {
                 content += '<tr>';
-                content += '<td>' + txn.name + '</td>';
-                content += '<td>$' + txn.amount + '</td>';
-                content += '<td>' + txn.date + '</td>';
+                content += '<td>' + account.name + '</td>';
+                content += '<td>$' + (account.balances.available != null ? account.balances.available : account.balances.current) + '</td>'
+                content += '<td>' + account.subtype + '</td>';
+                content += '<td>' + account.mask + '</td>';
                 content += '</tr>';
-                console.log(content);
             });
+        }).then(() => {
+            axios.get("http://localhost:5000/transactions").then(res => {
+                res.data.transactions.transactions.forEach(function(txn, idx) {
+                    content += '<tr>';
+                    content += '<td>' + txn.name + '</td>';
+                    content += '<td>$' + txn.amount + '</td>';
+                    content += '<td>' + txn.date + '</td>';
+                    content += '</tr>';
+                    console.log(content);
+                });
+            })
         }).then( () => {
             console.log(content); 
             this.setState({html: content})
@@ -28,8 +40,8 @@ export default class Info extends Component {
 
     render() {
         return (
-            <div>
-                {this.state.html}
+            <div dangerouslySetInnerHTML={{__html: this.state.html }}>
+                
             </div>
         );
     }
