0b0f1562e7c68554c7d20cbdc4d145b16008e20a
baileyg2016 <bspell20@vt.edu>
2020-02-17 21:12:57 -0500

    working on doing a big refactor

--- frontend/src/components/graph.component.js
+++ frontend/src/components/graph.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Doughnut, Bar} from 'react-chartjs-2';
 
 const wrapper = {
@@ -23,87 +23,97 @@ const right = {
     width: '450px',
 }
 
-const barOptions = {
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true,
-                callback: (value) => {if (value % 1 === 0) {return value;}}
-            }
-        }]
+var mBarOptions = {
+    options: {
+        title: {
+            display: true,
+            text: "Here is how often you spend money at these places: "
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: (value) => {if (value % 1 === 0) {return value;}}
+                }
+            }]
+        }
     }
 };
 
-export default class Graph extends Component {
-    constructor(props) {
-        super(props);
-        this.randomColor = this.randomColor.bind(this);
-        this.state = {
-            doughnutData: {},
-            barData: {}
-        };
-    }
-
-    randomColor() {
-        var rgb = [];
-
-        for(var i = 0; i < 3; i++)
-            rgb.push(Math.floor(Math.random() * 255));
-
-        return 'rgb('+ rgb.join(',') +')';
-    }
-
-    componentDidMount() {
-        var colors = []
-        for (var i = 0; i < this.props.doughnutValues.length; i++) {
-            colors.push(this.randomColor());
+var mDoughnutOptions = {
+    options: {
+        title: {
+            display: true,
+            text: 'Current Spending'
         }
-        var mDoughnutData = {
-            labels: this.props.doughnutLabels,
-            datasets: [{
-              label: "Your current spending",
-              backgroundColor: colors,
-              borderColor: 'rgb(255, 255, 255)',
-              data: this.props.doughnutValues,
-              borderWidth: 1
-            }],
-            options: {
-                title: {
-                    display: true,
-                    text: 'Current Spending'
-                }
-            }
-        }
-
-        var mBarData = {
-            labels: this.props.barLabels,
-            datasets: [{
-                label: "Frequency of Payments",    
-                backgroundColor: colors,
-                data: this.props.barValues,
-                minBarLength: 1
-            }]
-        }
-        this.setState({doughnutData: mDoughnutData, barData: mBarData});
     }
+};
 
-    render () {
-        return (
-            <div style={wrapper}>
-                <div style={right} >
-                    <Doughnut
-                        width={this.props.width} 
-                        height={this.props.height}
-                        data={this.state.doughnutData} />
-                </div>
-                <div style={left} >
-                    <Bar
-                        width={this.props.width} 
-                        height={this.props.height}
-                        data={this.state.barData}
-                        options={barOptions} />
-                </div>
+const Graph = (props) => {
+    // constructor(props) {
+    //     super(props);
+    //     this.randomColor = this.randomColor.bind(this);
+    //     this.state = {
+    //         doughnutData: {},
+    //         barData: {}
+    //     };
+    // }
+
+
+    // componentWillReceiveProps() {
+    //     var colors = []
+    //     for (var i = 0; i < this.props.doughnutValues.length; i++) {
+    //         colors.push(this.randomColor());
+    //     }
+    //     var mDoughnutData = {
+    //         labels: this.props.doughnutLabels,
+    //         datasets: [{
+    //           label: "Your current spending",
+    //           backgroundColor: colors,
+    //           borderColor: 'rgb(255, 255, 255)',
+    //           data: this.props.doughnutValues,
+    //           borderWidth: 1
+    //         }],
+    //         options: {
+    //             title: {
+    //                 display: true,
+    //                 text: 'Current Spending'
+    //             }
+    //         }
+    //     }
+
+    //     var mBarData = {
+    //         labels: this.props.barLabels,
+    //         datasets: [{
+    //             label: "Frequency of Payments",    
+    //             backgroundColor: colors,
+    //             data: this.props.barValues,
+    //             minBarLength: 1
+    //         }]
+    //     }
+    //     this.setState({doughnutData: mDoughnutData, barData: mBarData});
+    // }
+
+    
+    return (
+        <div style={wrapper}>
+            <div style={right} >
+                <Doughnut
+                    width={this.props.width} 
+                    height={this.props.height}
+                    data={this.props.doughnutData}
+                    options={mDoughnutOptions} />
             </div>
-        )
-    }
+            <div style={left} >
+                <Bar
+                    width={this.props.width} 
+                    height={this.props.height}
+                    data={this.props.barData}
+                    options={mBarOptions} />
+            </div>
+        </div>
+    )
+    
 }
+
+export default Graph;
